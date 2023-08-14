a822810a96a44043b2bd277d4e196876230691fb
Rohit Satwadhar <rvsatwadhar@gmail.com>
2023-08-15 01:53:50 +0530

    Initial Commit

---
+++ src/test/java/com/bluebook/restservices/bluebook/BluebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluebook.restservices.bluebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
