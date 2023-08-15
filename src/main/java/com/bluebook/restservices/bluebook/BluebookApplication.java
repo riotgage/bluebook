79b2e94b3d41a4ce6a71bcc7c078846c760ee95a
Rohit Satwadhar <rvsatwadhar@gmail.com>
2023-08-15 10:50:28 +0530

    Second Commit

--- src/main/java/com/bluebook/restservices/bluebook/BluebookApplication.java
+++ src/main/java/com/bluebook/restservices/bluebook/BluebookApplication.java
@@ -3,6 +3,7 @@ package com.bluebook.restservices.bluebook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// This is a test project for spring boot
 @SpringBootApplication
 public class BluebookApplication {
 
