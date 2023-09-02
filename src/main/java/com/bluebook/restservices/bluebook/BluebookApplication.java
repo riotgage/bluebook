8c0b8e35b6b3b4ccf4d2c62317964b4ed716ac1f
Rohit Satwadhar <rvsatwadhar@gmail.com>
2023-09-02 18:29:36 +0530

    Added Internationalization

--- src/main/java/com/bluebook/restservices/bluebook/BluebookApplication.java
+++ src/main/java/com/bluebook/restservices/bluebook/BluebookApplication.java
@@ -2,6 +2,12 @@ package com.bluebook.restservices.bluebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
 
 // This is a test project for spring boot
 @SpringBootApplication
@@ -11,4 +17,22 @@ public class BluebookApplication {
 		SpringApplication.run(BluebookApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver(){
+		
+		AcceptHeaderLocaleResolver localeResolver
+				=new AcceptHeaderLocaleResolver();
+		
+		localeResolver.setDefaultLocale(Locale.US);
+		
+		return localeResolver;
+		
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource messageSource(){
+		ResourceBundleMessageSource messageSource=new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return  messageSource;
+	}
 }
--- src/main/java/com/bluebook/restservices/bluebook/hello/HelloWorldController.java
+++ src/main/java/com/bluebook/restservices/bluebook/hello/HelloWorldController.java
@@ -1,15 +1,24 @@
 package com.bluebook.restservices.bluebook.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
 
 	// Method which will be mapped to URI
 	// GET Method and its path (URI)
+	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
 	
 	@RequestMapping(method=RequestMethod.GET,path="/")
 	public String test(){
@@ -26,4 +35,11 @@ public class HelloWorldController {
 		return new UserDetails("Rohit","Satadhar","Pune");
 	}
 	
+	@GetMapping("/hello-int")
+	public String getMessagesInI18nFormat(@RequestHeader(name="Accept-Language",required = false)String locale){
+		return messageSource.getMessage("label.hello",null,new Locale(locale));
+		
+		//		LocaleContextHolder.getLocale(); this can be used to get locale directly instead
+		//		of writing (@RequestHeader(name="Accept-Language",required = false) in method signature
+	}
 }
