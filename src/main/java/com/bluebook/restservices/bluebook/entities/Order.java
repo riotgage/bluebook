1711fa2d521454c174d8db2ee03a9634c9ba75bd
Rohit Satwadhar <rvsatwadhar@gmail.com>
2023-09-12 21:30:00 +0530

    Added jackson dataformat dependency to support xml data accept and xml data return

--- src/main/java/com/bluebook/restservices/bluebook/entities/Order.java
+++ src/main/java/com/bluebook/restservices/bluebook/entities/Order.java
@@ -52,10 +52,6 @@ public class Order extends RepresentationModel {
 		this.orderDescription = orderDescription;
 	}
 	
-	public User getUser() {
-		return user;
-	}
-	
 	public void setUser(User user) {
 		this.user = user;
 	}
